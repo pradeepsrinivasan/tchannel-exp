48d34ac6d57a3a3a92bd346ae5ae707197f230dd
Pradeep S <pradeep@flipkart.com>
2016-02-01 13:55:49 +0530

    codec additions

---
+++ tchannel-models/src/main/java/pradeep/exp/tchannel/models/codec/RequestCodec.java
@@ -0,0 +1,20 @@
+package pradeep.exp.tchannel.models.codec;
+
+import pradeep.exp.tchannel.models.fb.Request;
+
+/**
+ * Created by pradeep on 01/02/16.
+ */
+public class RequestCodec {
+
+    public String encode(Request request) {
+
+        return null;
+    }
+
+    public Request decode(String request) {
+
+        return null;
+    }
+
+}
---
+++ tchannel-models/src/main/java/pradeep/exp/tchannel/models/codec/ResponseCodec.java
@@ -0,0 +1,20 @@
+package pradeep.exp.tchannel.models.codec;
+
+import pradeep.exp.tchannel.models.fb.Response;
+
+/**
+ * Created by pradeep on 01/02/16.
+ */
+public class ResponseCodec {
+
+    public String encode(Response response) {
+
+        return null;
+    }
+
+    public Response decode(String request) {
+
+        return null;
+    }
+
+}
